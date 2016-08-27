d858febfc7b53507ed80790ee5d3662b86e0c1e4
Mezov <andrey.mezov@gmail.com>
2016-08-27 19:05:13 +0300

    Update 5.8

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('APP_ROOT', '/blog');
+define('APP_ROOT', '/Practical-Project-Softuni');
 define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define('DB_NAME', 'photoshoot');
